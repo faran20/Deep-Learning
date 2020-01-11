d97bb26499cccc9a8ba5b63172cd9e23854e4ae1
faran20 <f138196@nu.edu.pk>
2020-01-11 23:36:33 +0400

    Update numpy function

--- Logistic Regression to Predict Cat in Images.py
+++ Logistic Regression to Predict Cat in Images.py
@@ -43,30 +43,9 @@ def sigmoid(z):
     return s
 
 
-def initialize_with_zeros(dim):
-    """
-    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
-    Argument:
-    dim -- size of the w vector we want (or number of parameters in this case)
-    Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias)
-    """
-
-    ### START CODE HERE ### (≈ 1 line of code)
-    w = np.zeros((dim, 1))
-    b = 0
-    ### END CODE HERE ###
-
-    assert (w.shape == (dim, 1))
-    assert (isinstance(b, float) or isinstance(b, int))
-
-    return w, b
-
-
 def propagate(w, b, X, Y):
     """
-    Implement the cost function and its gradient for the propagation explained above
+    Implement the cost function and its gradient for the propagation
     Arguments:
     w -- weights, a numpy array of size (num_px * num_px * 3, 1)
     b -- bias, a scalar
@@ -76,24 +55,17 @@ def propagate(w, b, X, Y):
     cost -- negative log-likelihood cost for logistic regression
     dw -- gradient of the loss with respect to w, thus same shape as w
     db -- gradient of the loss with respect to b, thus same shape as b
-    Tips:
-    - Write your code step by step for the propagation. np.log(), np.dot()
     """
 
     m = X.shape[1]
 
     # FORWARD PROPAGATION (FROM X TO COST)
-    ### START CODE HERE ### (≈ 2 lines of code)
     A = sigmoid(np.dot(w.T, X) + b)
-    something = np.multiply(Y, np.log(A)) + np.multiply((1 - Y), np.log(1 - A))
-    cost = -np.sum(something, axis=1, keepdims=True) / m
-    ### END CODE HERE ###
+    cost = -np.sum(np.multiply(Y, np.log(A)) + np.multiply((np.subtract(1,Y)), np.log(np.subtract(1,A))), axis=1, keepdims=True) / m
 
     # BACKWARD PROPAGATION (TO FIND GRAD)
-    ### START CODE HERE ### (≈ 2 lines of code)
-    dw = np.dot(X, (A - Y).T) / m
-    db = np.sum((A - Y), axis=1, keepdims=True) / m
-    ### END CODE HERE ###
+    dw = np.dot(X, (np.subtract(A,Y)).T) / m
+    db = np.sum((np.subtract(A,Y)), axis=1, keepdims=True) / m
 
     assert (dw.shape == w.shape)
     assert (db.dtype == float)
@@ -121,29 +93,24 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
     params -- dictionary containing the weights w and bias b
     grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
     costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    Tips:
-    You basically need to write down two steps and iterate through them:
-        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+    Funtion to:
+        1) Calculate the cost and the gradient for the current parameters. Using propagate().
         2) Update the parameters using gradient descent rule for w and b.
     """
 
     costs = []
     for i in range(num_iterations):
 
-        # Cost and gradient calculation (≈ 1-4 lines of code)
-        ### START CODE HERE ###
+        # Cost and gradient calculation
         grads, cost = propagate(w, b, X, Y)
-        ### END CODE HERE ###
 
         # Retrieve derivatives from grads
         dw = grads["dw"]
         db = grads["db"]
 
-        # update rule (≈ 2 lines of code)
-        ### START CODE HERE ###
+        # update rule
         w = w - learning_rate * dw
         b = b - learning_rate * db
-        ### END CODE HERE ###
 
         # Record the costs
         if i % 100 == 0:
@@ -178,22 +145,15 @@ def predict(w, b, X):
     w = w.reshape(X.shape[0], 1)
 
     # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    ### START CODE HERE ### (≈ 1 line of code)
     A = sigmoid(np.dot(w.T, X) + b)
-    ### END CODE HERE ###
-
-
 
     for i in range(A.shape[1]):
-
         # Convert probabilities A[0,i] to actual predictions p[0,i]
-        ### START CODE HERE ### (≈ 4 lines of code)
         if A[0][i] > 0.5:
             Y_prediction[0][i] = 1
         else:
             Y_prediction[0][i] = 0
         pass
-        ### END CODE HERE ###
 
     assert (Y_prediction.shape == (1, m))
 
@@ -202,7 +162,7 @@ def predict(w, b, X):
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
     """
-    Builds the logistic regression model by calling the function you've implemented previously
+    Builds the logistic regression model by calling the function
     Arguments:
     X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
     Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
@@ -217,23 +177,22 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
 
     ### START CODE HERE ###
 
-    # initialize parameters with zeros (≈ 1 line of code)
+    # initialize parameters with zeros
     w, b = np.zeros((X_train.shape[0], 1)), 0
 
-    # Gradient descent (≈ 1 line of code)
+    # Gradient descent
     parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
 
     # Retrieve parameters w and b from dictionary "parameters"
     w = parameters["w"]
     b = parameters["b"]
 
-    # Predict test/train set examples (≈ 2 lines of code)
+    # Predict test/train set example
     Y_prediction_test = predict(w, b, X_test)
     Y_prediction_train = predict(w, b, X_train)
 
-    ### END CODE HERE ###
 
-    # Print train/test Errors
+    # Print train/test Result
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
     print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
 
